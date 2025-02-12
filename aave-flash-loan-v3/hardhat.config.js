c01cc4a7dc1d772f10b8bb2219a26c510d76bee0
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2025-02-12 13:25:09 -0800

    single source ENV

--- aave-flash-loan-v3/hardhat.config.js
+++ aave-flash-loan-v3/hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
+// require("dotenv").config();
+require('dotenv').config({path: '../.env'});
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
